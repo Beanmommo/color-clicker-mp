7207f6e93a025041cbc3140a5bbc57cacf2a0c2c
beanmommo <leonardo.prasetyo5@gmail.com>
2024-11-01 16:34:08 +1100

    Added name input on main screen

--- components/MainPage.vue
+++ components/MainPage.vue
@@ -1,3 +1,10 @@
+<script setup lang="ts">
+const name = ref<string>()
+function clickHandlerJoin() {
+    console.log(name.value)
+}
+</script>
+
 <template>
     <div>
         <h1>
@@ -5,8 +12,11 @@
         </h1>
         <h4>Collaborate to Win!!</h4>
         <div>
+            <input v-model="name" type="text" />
+        </div>
+        <div>
             <button>Host Room</button>
-            <button>Join Room</button>
+            <button @click="clickHandlerJoin">Join Room</button>
         </div>
     </div>
 
